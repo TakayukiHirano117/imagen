95c9281047ddb7fd92dec20123944588cb11f27d
hirano-ta <hirano-ta@ibjapan.jp>
2025-06-02 17:51:06 +0900

    画像生成処理のサンプルを実装

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,8 @@
+require 'httparty'
+
 class ImagesController < ApplicationController
+  include HTTParty
+
   def index
   end
 
@@ -6,6 +10,27 @@ class ImagesController < ApplicationController
   end
 
   def new
+    base_url = "https://api.openai.com/v1/images/generations"
+    api_key = ENV["OPENAI_API_KEY"]
+
+    headers = {
+      "Authorization" => "Bearer #{api_key}",
+      "Content-Type" => "application/json"
+    }
+    
+    response = HTTParty.post(base_url, headers: headers, body: {
+      model: "gpt-image-1",
+      prompt: "A beautiful sunset over a calm ocean",
+      n: 1,
+      size: "1024x1024",
+    }.to_json)
+
+    if response.success?
+      @base64_image = response.dig("data", 0, "b64_json")
+    else
+      Rails.logger.error "OpenAI API Error: #{response.body}"
+      @base64_image = nil
+    end
   end
 
   def create
