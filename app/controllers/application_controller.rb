342644142798203c9503121e219f8a39dc4b2df9
hirano-ta <hirano-ta@ibjapan.jp>
2025-06-02 13:39:39 +0900

    deviseの新規登録ビューを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  # 新規登録後の遷移先をカスタマイズ
+  def after_sign_up_path_for(resource)
+    images_path
+  end
+
+  # ログイン後の遷移先をカスタマイズ（必要に応じて）
+  def after_sign_in_path_for(resource)
+    images_path
+  end
 end
