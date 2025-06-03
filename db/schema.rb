a042e5b62376446e53493742837d91c6af51d8ba
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-06-03 13:20:07 +0900

    ✨ スキーマを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_02_022951) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_02_124103) do
+  create_table "images", force: :cascade do |t|
+    t.string "cloudinary_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
